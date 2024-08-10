85f7f5cb17a9c65cebf2f6bc2abf2ac656d9eac4
serhat111 <49848798+serhat111@users.noreply.github.com>
2024-08-10 00:40:33 -0500

    Revert "beta1"
    This reverts commit e7b89a307bd4f3a2e1ef2a1ef8233a7d56a8271b.

--- src/components/chatList/ChatList.jsx
+++ src/components/chatList/ChatList.jsx
@@ -15,8 +15,8 @@ const ChatList = () => {
     <div className="chatList">
       <span className="title">DASHBOARD</span>
       <Link to="/dashboard">Create a new Chat</Link>
-      {/* <Link to="/">Explore Oura AI</Link>
-      <Link to="/">Contact</Link> */}
+      <Link to="/">Explore Lama AI</Link>
+      <Link to="/">Contact</Link>
       <hr />
       <span className="title">RECENT CHATS</span>
       <div className="list">
@@ -34,7 +34,7 @@ const ChatList = () => {
       <div className="upgrade">
         <img src="/logo.png" alt="" />
         <div className="texts">
-          <span>Upgrade to Oura AI Pro</span>
+          <span>Upgrade to Lama AI Pro</span>
           <span>Get unlimited access to all features</span>
         </div>
       </div>
